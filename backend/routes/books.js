fa6e0728d4ba528052fc14b53ad113200adfff2e
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-07-03 19:01:01 -0300

    Backeend finished, Rest API :D

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -8,4 +8,16 @@ router.get("/", async (req, res) => {
   res.json(books);
 });
 
+router.post("/", async (req, res) => {
+  const { title, author, isbn } = req.body;
+  const newBook = new Book({ title, author, isbn });
+  await newBook.save();
+  res.json({ message: "Book Saved" });
+});
+
+router.delete("/:id", async (req, res) => {
+  await Book.findByIdAndDelete(req.params.id);
+  res.json({ message: "Book deleted" });
+});
+
 module.exports = router;
